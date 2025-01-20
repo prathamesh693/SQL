adb3cbda7117708179e3f2004a92140b78380f1c
Jadhav Prathamesh Suresh <prathamehj693@gmail.com>
2025-01-20 18:46:06 +0530

    Window Function

--- SQL_Basic/Jan_16.sql
+++ SQL_Basic/Jan_16.sql
@@ -3,20 +3,31 @@
 
 -- Using sub-query retrive the avg,min,max and count of and age
 SELECT * FROM (
-SELECT gender,avg(age),min(age),max(age),count(age)
-FROM sql_basic.employee_demographics
-GROUP BY gender) AS Agg_table
+SELECT Gender,avg(Age),min(Age),max(Age),count(Age)
+FROM sql_basic.Employee_Demographics
+GROUP BY Gender) AS Agg_table
 -- Group by gender 
 ;
 
 -- Use the output of one function to the others and perform operations on it.
-SELECT gender, AVG('max(age)') FROM (
+SELECT Gender, AVG('max(Age)') FROM (
 -- If we can't use inverted commas then it consider it as a function and 
 -- gives an error to the query.
-SELECT gender,avg(age),min(age),max(age),count(age)
-FROM sql_basic.employee_demographics
-GROUP BY gender) AS Agg_table
-GROUP BY gender
-;
+SELECT Gender,avg(Age)
+FROM sql_basic.Employee_Demographics
+GROUP BY Gender) AS Agg_table
+GROUP BY Gender;
 
---
+-- OVER() Window function
+
+-- Calculate the average salary for each gender using the OVER() window function.
+-- PARTITION BY Gender groups the rows by gender for the calculation.
+SELECT dem.Gender, AVG(sal.Salary) OVER(PARTITION BY Gender ) AS AVG_SALARY
+FROM Employee_Demographics AS dem
+JOIN Employee_Salary AS sal ON dem.Employee_ID = sal.Employee_ID;
+
+-- Calculate the overall average salary across all employees using the OVER() window function.
+-- Without PARTITION BY, the calculation considers all rows as a single group.
+SELECT dem.Gender, AVG(sal.Salary) OVER()
+FROM Employee_Demographics AS dem
+JOIN Employee_Salary AS sal ON dem.Employee_ID = sal.Employee_ID;
